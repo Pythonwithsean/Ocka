e0b035633358d2a1ee068a4c1d99e80297702fbb
pythonwithsean <pythonwithsean@gmail.com>
2024-07-25 19:06:11 +0100

    mend
    [update] add auth,signup,login: routes,controller, api

--- src/backend/routes/auth-route.ts
+++ src/backend/routes/auth-route.ts
@@ -1,7 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import authController from "../controllers/auth-controller";
 
 
 
-export default async function authRoutes(req: NextApiRequest, res: NextApiResponse) {
-
+export default async function authRoutes(req: NextApiRequest, res: NextApiResponse): Promise<void> {
+	await authController(req, res)
 }
