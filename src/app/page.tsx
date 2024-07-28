0cf769eb6cc6c9f918f30db881d23d1b3d407307
pythonwithsean <pythonwithsean@gmail.com>
2024-07-28 10:54:10 +0100

    add responsive image frame

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,95 +6,78 @@ export default async function Home() {
     <main>
       <Header />
       <img src="/images/blob.svg" alt="Blob" className="blob" />
-      <section
-        className="Hero"
-        style={{
-          borderBottom: '5px solid red',
-        }}
-      >
-        <h1
-          className=" align-middle font-bold font-mono"
+      <main className="Hero-container">
+        <section
+          className="Hero"
           style={{
-            fontSize: '3.5rem',
-            lineHeight: '1.3',
-            lineClamp: 2,
-            textAlign: 'center',
-            fontWeight: 800,
-            color: 'rgb(0, 0, 0)',
+            borderBottom: '5px solid red',
           }}
         >
-          The best way to Generate a{' '}
-          <span
+          <h1
+            className=" align-middle font-bold font-mono"
             style={{
-              color: 'rgb(255, 0, 0)',
+              fontSize: '3.5rem',
+              lineHeight: '1.3',
+              lineClamp: 2,
+              textAlign: 'center',
+              fontWeight: 800,
+              color: 'rgb(0, 0, 0)',
             }}
           >
-            resume
-          </span>
+            The best way to Generate a{' '}
+            <span
+              style={{
+                color: 'rgb(255, 0, 0)',
+              }}
+            >
+              resume
+            </span>
+            <br />
+            with
+            <span
+              style={{
+                color: 'rgb(255, 0, 0)',
+              }}
+            >
+              {' '}
+              Ai{' '}
+            </span>
+            in seconds!
+          </h1>
           <br />
-          with
-          <span
+          <section
+            className="Hero"
             style={{
-              color: 'rgb(255, 0, 0)',
+              marginTop: '10px',
             }}
           >
-            {' '}
-            Ai{' '}
-          </span>
-          in seconds!
-        </h1>
-        <br />
-      </section>
-      <section
-        className="image-container"
-        style={{
-          position: 'absolute',
-          right: '20%',
-          top: '10%',
-        }}
-      >
-        <VideoEmbed />
-      </section>
-      <section
-        className="Hero"
-        style={{
-          marginTop: '10px',
-        }}
-      >
-        <p
-          style={{
-            fontSize: '1rem',
-            lineHeight: '1.4',
-            lineClamp: 2,
-            textAlign: 'center',
-            fontWeight: 500,
-            color: 'rgb(0, 0, 0)',
-          }}
-        >
-          What is Ocka? Ocka is a resume generator that uses Ai to generate
-          resumes in seconds.
-        </p>
-        <p
-          style={{
-            fontSize: '1rem',
-            lineHeight: '1.4',
-            lineClamp: 2,
-            textAlign: 'center',
-            fontWeight: 500,
-            color: 'rgb(0, 0, 0)',
-          }}
-        >
-          With{' '}
-          <span
-            style={{
-              color: 'red',
-            }}
-          >
-            Ocka
-          </span>{' '}
-          you can have your CV Done in seconds
-        </p>
-      </section>
+            <p
+              style={{
+                fontSize: '1rem',
+                lineHeight: '1.4',
+                lineClamp: 2,
+                textAlign: 'center',
+                fontWeight: 500,
+                color: 'rgb(0, 0, 0)',
+              }}
+            >
+              What is{' '}
+              <span
+                style={{
+                  color: 'red',
+                }}
+              >
+                Ocka?{' '}
+              </span>
+              Ocka is a resume generator that uses Ai to generate resumes in
+              seconds.
+            </p>
+          </section>
+        </section>
+        <section className="video-container">
+          <VideoEmbed />
+        </section>
+      </main>
     </main>
   );
 }
