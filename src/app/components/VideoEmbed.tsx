0c8a2440efe331aa1f84c547bc4eb6281983cb7b
pythonwithsean <pythonwithsean@gmail.com>
2024-07-28 10:30:34 +0100

    [update] video frame add

--- src/app/components/VideoEmbed.tsx
+++ src/app/components/VideoEmbed.tsx
@@ -1,20 +1,26 @@
 'use client';
-import { LegacyRef, useRef } from 'react';
+import { useLayoutEffect, useRef } from 'react';
 export default function VideoEmbed() {
   type videoReftype = HTMLVideoElement | undefined;
   const videoRef = useRef<videoReftype>();
+  useLayoutEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.loop = true;
+      videoRef.current.play().then(() => {
+        console.log('Video Playing');
+      });
+    }
+  }, []);
   return (
     <>
       <video
+        className="Frame"
         src="/videos/demo1.mp4"
         autoPlay={true}
+        loop={true}
         ref={videoRef as any}
-        onLoad={() => {
-          console.log('Video Loaded');
-        }}
-      >
-        Hello
-      </video>
+        muted={true}
+      ></video>
     </>
   );
 }
