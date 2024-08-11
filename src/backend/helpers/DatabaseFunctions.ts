7e6ef9ddc54e27e482702a378e17189438550670
pythonwithsean <pythonwithsean@gmail.com>
2024-08-11 20:09:46 +0100

    [update] I USE ARCH BTW

--- src/backend/helpers/DatabaseFunctions.ts
+++ src/backend/helpers/DatabaseFunctions.ts
@@ -1,15 +1,52 @@
 import mysql from "mysql2"
 import { compareHash } from "./hashing"
 
-export async function loginUser(username: string, password: string, connection: mysql.Connection): Promise<{
-  user: string,
-  id: string
-}> {
+
 
 export async function loginUser(username: string, password: string, connection: mysql.Connection): Promise<{
   user: string,
   id: string
 }> {
+  return new Promise((res) => {
+    try {
+      const query = `SELECT id, username, password FROM Users where username = ?`
+      connection.execute(query, [username], (err, result) => {
+        if ((result as []).length >= 1) {
+          const foo = result[0] as { username: string, password: string, id: string };
+          compareHash(password, foo.password).then(valid => {
+            if (valid === true) {
+              res({
+                id: foo.id,
+                user: foo.username,
+              })
+              return
+            } else {
+              console.error("No User Found")
+              res({
+                id: "",
+                user: ""
+              })
+              return
+            }
+          })
+        } else {
+          console.error("No User Found")
+          res({
+            id: "",
+            user: "",
+          })
+          return
+        }
+      })
+    } catch (err: any) {
+      console.error("Error from try catch")
+      console.error(err.message)
+      res({
+        id: "",
+        user: ""
+      })
+    }
+  })
 }
 
 export async function createUser(id: string, email: string, username: string, password: string, connection: mysql.Connection): Promise<boolean> {
